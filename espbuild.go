288853b7c067f57c5321037139863e36dd075cce
Eric Molitor <eric@molitor.org>
2019-05-19 13:52:31 +0100

    Stream stdout and stderr

--- espbuild.go
+++ espbuild.go
@@ -2,10 +2,13 @@ package main
 
 import "go.starlark.net/starlark"
 
+import "bufio"
 import "fmt"
 import "log"
 import "os"
 import "os/exec"
+import "strings"
+import "sync"
 
 func toString(arg starlark.Value) (string) {
   argStr,isString := starlark.AsString( arg )
@@ -17,10 +20,67 @@ func toString(arg starlark.Value) (string) {
 }
 
 func shell(arg string) {
-  out, err := exec.Command("/bin/sh", "-xec", arg).CombinedOutput()
-  fmt.Printf("%s", out)
+  //out, err := exec.Command("/bin/sh", "-xec", arg).CombinedOutput()
+  //fmt.Printf("%s", out)
 
+  //if err != nil {
+  //  log.Fatal(err)
+  //}
+
+  var wg sync.WaitGroup
+
+  cmd := exec.Command("/bin/sh", "-xec", arg)
+
+  stdout, err := cmd.StdoutPipe()
   if err != nil {
+    log.Fatal(err)
+  }
+  stderr, err := cmd.StderrPipe()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  if err := cmd.Start(); err != nil {
+    log.Fatal(err)
+  }
+
+  outch := make(chan string, 10)
+
+  scannerStdout := bufio.NewScanner(stdout)
+  scannerStdout.Split(bufio.ScanLines)
+  wg.Add(1)
+  go func() {
+    for scannerStdout.Scan() {
+        text := scannerStdout.Text()
+        if strings.TrimSpace(text) != "" {
+            outch <- text
+        }
+    }
+    wg.Done()
+  }()
+  scannerStderr := bufio.NewScanner(stderr)
+  scannerStderr.Split(bufio.ScanLines)
+  wg.Add(1)
+  go func() {
+    for scannerStderr.Scan() {
+        text := scannerStderr.Text()
+        if strings.TrimSpace(text) != "" {
+            outch <- text
+        }
+    }
+    wg.Done()
+  }()
+
+  go func() {
+    wg.Wait()
+    close(outch)
+  }()
+
+  for t := range outch {
+    fmt.Println(t)
+  }
+
+  if err := cmd.Wait(); err != nil {
     log.Fatal(err)
   }
 }
