23723be613ba59bf2636cf4aad4741c9ee4742fc
Eric Molitor <eric@molitor.org>
2020-03-29 13:48:36 +0100

    Reset espbuild

---
+++ get.go
@@ -0,0 +1,82 @@
+package main
+
+import "fmt"
+import "io"
+import "log"
+import "net"
+import "net/http"
+import "os"
+import "strings"
+import "time"
+
+import "gopkg.in/src-d/go-git.v4"
+
+func getHttp(url string) {
+  urlSplit := strings.Split(url, "/")
+  outputFile := urlSplit[len(urlSplit)-1]
+
+  var netTransport = &http.Transport{
+    Dial: (&net.Dialer{
+      Timeout: 5 * time.Second,
+    }).Dial,
+    TLSHandshakeTimeout: 5 * time.Second,
+  }
+
+  var netClient = &http.Client{
+    Timeout: time.Second * 10,
+    Transport: netTransport,
+  }
+
+  resp, err := netClient.Get(url)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer resp.Body.Close()
+  out, err := os.Create(outputFile)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer out.Close()
+  io.Copy(out, resp.Body)
+}
+
+func getGit(url string, outputDir string) {
+  _, err := git.PlainClone(outputDir, false, &git.CloneOptions{
+    URL: url,
+  })
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+func getGitInferred(url string) {
+  urlSplit := strings.Split(url, "/")
+  outputDir := urlSplit[len(urlSplit)-1]
+  getGit(url, outputDir)
+}
+
+func main() {
+  //getHttp(os.Args[1], os.Args[2])
+
+  if len(os.Args) < 2 {
+    fmt.Println("Usage:")
+    fmt.Println("\tget https://example.com/filename.tar.gz")
+    fmt.Println("\tget http https://example.com/filename.tar.gz")
+    fmt.Println("\tget git https://exmample.com/git/url")
+  } else {
+    switch os.Args[1] {
+    case "git":
+      if len(os.Args) > 2 {
+        getGit(os.Args[2], os.Args[3])
+      } else {
+        getGitInferred(os.Args[2])
+      }
+    default:
+      if (os.Args[1] == "http") {
+        getHttp(os.Args[2])
+      } else {
+        getHttp(os.Args[1])
+      }
+    }
+  }
+}
--- go.mod
+++ go.mod
@@ -1,14 +1,5 @@
 module espbuild
 
-go 1.12
+go 1.13
 
-require (
-	github.com/dsnet/compress v0.0.1
-	github.com/dustin/go-humanize v1.0.0
-	github.com/ulikunitz/xz v0.5.6
-	go.starlark.net v0.0.0-20190702223751-32f345186213
-	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
-	golang.org/x/text v0.3.2 // indirect
-	gopkg.in/src-d/go-billy.v4 v4.3.1 // indirect
-	gopkg.in/src-d/go-git.v4 v4.12.0
-)
+require gopkg.in/src-d/go-git.v4 v4.12.0
