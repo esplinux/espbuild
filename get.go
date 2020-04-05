3b9bf3068aad88a21f2526bb9d62135a05075650
Eric Molitor <eric@molitor.org>
2020-04-05 11:40:21 +0100

    Add support for untarring and decompression

--- get.go
+++ get.go
@@ -1,17 +1,34 @@
 package main
 
-import "fmt"
-import "io"
-import "log"
-import "net"
-import "net/http"
-import "os"
-import "strings"
-import "time"
+import (
+	"archive/tar"
+	"compress/bzip2"
+	"compress/gzip"
+	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/ulikunitz/xz"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
 
-import "github.com/go-git/go-git/v5"
+func handle(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-func getHttp(url string) {
+func closer(closer io.Closer) {
+	err := closer.Close()
+	handle(err)
+}
+
+func getHttpSource(url string, outputDir string) {
 	urlSplit := strings.Split(url, "/")
 	outputFile := urlSplit[len(urlSplit)-1]
 
@@ -28,44 +45,85 @@ func getHttp(url string) {
 	}
 
 	resp, err := netClient.Get(url)
-	if err != nil {
-		log.Fatal(err)
+	handle(err)
+	defer closer(resp.Body)
+
+	var reader io.Reader
+	if strings.HasSuffix(outputFile, ".gz") {
+		gzipStream, err := gzip.NewReader(resp.Body)
+		handle(err)
+		defer closer(gzipStream)
+		reader = gzipStream
+	} else if strings.HasSuffix(outputFile, "bz") {
+		reader = bzip2.NewReader(resp.Body)
+	} else if strings.HasSuffix(outputFile, "xz") {
+		reader, err = xz.NewReader(resp.Body)
+		handle(err)
+	} else {
+		reader = resp.Body
 	}
 
-	out, err := os.Create(outputFile)
-	if err != nil {
-		log.Fatal(err)
-	}
+	tr := tar.NewReader(reader)
+	for {
+		header, err := tr.Next()
 
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+		switch {
 
-	err = resp.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
+		// if no more files are found return
+		case err == io.EOF:
+			return
 
-	err = out.Close()
-	if err != nil {
-		log.Fatal(err)
+		// return any other error
+		case err != nil:
+			handle(err)
+
+		// if the header is nil, just skip it (not sure how this happens)
+		case header == nil:
+			continue
+		}
+
+		// the target location where the dir/file should be created
+		target := filepath.Join(outputDir, header.Name)
+
+		// the following switch could also be done using fi.Mode(), not sure if there
+		// a benefit of using one vs. the other.
+		// fi := header.FileInfo()
+
+		// check the file type
+		switch header.Typeflag {
+
+		// if its a dir and it doesn't exist create it
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				err = os.MkdirAll(target, 0755)
+				handle(err)
+			}
+
+		// if it's a file create it
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			handle(err)
+
+			// copy over contents
+			_, err = io.Copy(f, tr)
+			handle(err)
+
+			// manually close here after each file operation; deferring would cause each file close
+			// to wait until all operations have completed.
+			err = f.Close()
+			handle(err)
+		}
 	}
 }
 
 func getGit(url string, outputDir string) {
+	urlSplit := strings.Split(url, "/")
+	outputDir = outputDir + "/" + urlSplit[len(urlSplit)-1]
+
 	_, err := git.PlainClone(outputDir, false, &git.CloneOptions{
 		URL: url,
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func getGitInferred(url string) {
-	urlSplit := strings.Split(url, "/")
-	outputDir := urlSplit[len(urlSplit)-1]
-	getGit(url, outputDir)
+	handle(err)
 }
 
 func main() {
@@ -79,16 +137,12 @@ func main() {
 	} else {
 		switch os.Args[1] {
 		case "git":
-			if len(os.Args) > 3 {
-				getGit(os.Args[2], os.Args[3])
-			} else {
-				getGitInferred(os.Args[2])
-			}
+			getGit(os.Args[2], ".")
 		default:
 			if os.Args[1] == "http" {
-				getHttp(os.Args[2])
+				getHttpSource(os.Args[2], ".")
 			} else {
-				getHttp(os.Args[1])
+				getHttpSource(os.Args[1], ".")
 			}
 		}
 	}
--- go.mod
+++ go.mod
@@ -1,7 +1,9 @@
 module espbuild
 
-go 1.14
+go 1.13
 
 require (
 	github.com/go-git/go-git/v5 v5.0.0
+	github.com/ulikunitz/xz v0.5.7
+	go.starlark.net v0.0.0-20200330013621-be5394c419b6
 )
